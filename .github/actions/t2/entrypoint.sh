e773d41623dfa9de4d43f8493e41f8a309fd9c0e
JackTheOG1 <japed33@gmail.com>
2026-04-28 21:42:12 -0500

    Update entrypoint.sh

--- .github/actions/t2/entrypoint.sh
+++ .github/actions/t2/entrypoint.sh
@@ -2,12 +2,8 @@
 
 set -e
 
-export ARM_CLIENT_ID=${INPUT_ARM_CLIENT_ID}
-export ARM_CLIENT_SECRET=${INPUT_ARM_CLIENT_SECRET}
-export ARM_SUBSCRIPTION_ID=${INPUT_ARM_SUBSCRIPTION_ID}
-export ARM_TENANT_ID=${INPUT_ARM_TENANT_ID}
 
-cd /github/workspace
+cd /github/workspace/t1
 ls -al
 
 echo "${INPUT_COMMENTER}"
