45fe98b8003d5982b7cb3cb68e16c68d59737760
JackTheOG1 <japed33@gmail.com>
2026-04-28 22:49:01 -0500

    Update entrypoint.sh

--- .github/actions/t2/entrypoint.sh
+++ .github/actions/t2/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
 set -e
 
+export ARM_CLIENT_ID="${INPUT_ARM_CLIENT_ID}"
+export ARM_CLIENT_SECRET="${INPUT_ARM_CLIENT_SECRET}"
+export ARM_SUBSCRIPTION_ID="${INPUT_ARM_SUBSCRIPTION_ID}"
+export ARM_TENANT_ID="${INPUT_ARM_TENANT_ID}"
+export STATE_KEY="${INPUT_STATE_KEY}"
+export TF_STAGE="${INPUT_TF_STAGE}"
+export DJANGO_SECRET_KEY_PROD="${INPUT_DJANGO_SECRET_KEY_PROD}"
 
-cd /github/workspace/t1
-ls -al
-
-export ARM_USE_CLI=false
-
-terraform init
-terraform plan -out=tfplan
-terraform apply -auto-approve tfplan
+terraform -chdir="${TF_STAGE}" init -backend-config="key=${STATE_KEY}.tfstate"
+terraform -chdir="${TF_STAGE}" plan -out="${TF_STAGE}.tfplan"
+terraform -chdir="${TF_STAGE}" apply -auto-approve "${TF_STAGE}.tfplan"
