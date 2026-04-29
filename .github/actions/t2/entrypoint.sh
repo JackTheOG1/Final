e9c7be72392508cc6c1c7928f728cf64fb36818c
JackTheOG1 <japed33@gmail.com>
2026-04-28 19:14:48 -0500

    Create entrypoint.sh

---
+++ .github/actions/t2/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+export ARM_CLIENT_ID=${INPUT_ARM_CLIENT_ID}
+export ARM_CLIENT_SECRET=${INPUT_ARM_CLIENT_SECRET}
+export ARM_SUBSCRIPTION_ID=${INPUT_ARM_SUBSCRIPTION_ID}
+export ARM_TENANT_ID=${INPUT_ARM_TENANT_ID}
+
+cd /github/workspace
+ls -al
+
+echo "${INPUT_COMMENTER}"
+
+terraform init
+terraform plan -out=tfplan -var "commenter=${INPUT_COMMENTER}"
+terraform apply -auto-approve tfplan
