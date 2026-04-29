2e5cc37f17542ce99583c5d078e306abfe1dc6c4
JackTheOG1 <japed33@gmail.com>
2026-04-29 16:16:16 -0500

    Update main.tf

--- t2/main.tf
+++ t2/main.tf
@@ -1,21 +1,22 @@
-variable ARM_CLIENT_ID {}
-variable ARM_CLIENT_SECRET {}
-variable DJANGO_SECRET_KEY {}
+variable "ARM_CLIENT_ID" {}
+variable "ARM_CLIENT_SECRET" {}
+variable "DJANGO_SECRET_KEY_PROD" {}
 
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
+      source = "hashicorp/azurerm"
       version = "4.70.0"
     }
   }
+
   backend "azurerm" {
-  resource_group_name = "rg-acmp-final"
-  storage_account_name = "acmp2400storageaccount"
-  container_name = "big-tf-state-acmp2400"
-  key = "jackpederson.tfstate"
-  use_azuread_auth = true
-}
+    resource_group_name = "rg-acmp-final"
+    storage_account_name = "acmp2400storageaccount"
+    container_name = "big-tf-state-acmp2400"
+    key = "jackpederson.tfstate"
+    use_azuread_auth = true
+  }
 }
 
 provider "azurerm" {
@@ -39,22 +40,23 @@ resource "azurerm_container_group" "teacher-aci" {
   os_type = "Linux"
 
   container {
-    name   = "final"
-    image  = "jackpedersonacmp2400.azurecr.io/final:latest"
-    cpu    = "0.5"
+    name = "final"
+    image = "acrjackpederson2400.azurecr.io/final:latest"
+    cpu = "0.5"
     memory = "1.5"
 
     ports {
-      port     = 8000
+      port = 8000
       protocol = "TCP"
     }
+
     secure_environment_variables = {
-      "DJANGO_SECRET_KEY" = var.DJANGO_SECRET_KEY_PROD
+      DJANGO_SECRET_KEY = var.DJANGO_SECRET_KEY_PROD
     }
   }
 
   image_registry_credential {
-    server   = "jackpedersonacmp2400.azurecr.io"
+    server = "acrjackpederson2400.azurecr.io"
     username = var.ARM_CLIENT_ID
     password = var.ARM_CLIENT_SECRET
   }
