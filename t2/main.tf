2586aabec10b8899c8191d1eba2ecbb75c516b9b
JackTheOG1 <japed33@gmail.com>
2026-04-29 15:30:15 -0500

    Create main.tf

---
+++ t2/main.tf
@@ -0,0 +1,61 @@
+variable ARM_CLIENT_ID {}
+variable ARM_CLIENT_SECRET {}
+variable DJANGO_SECRET_KEY {}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.70.0"
+    }
+  }
+  backend "azurerm" {
+  resource_group_name = "rg-acmp-final"
+  storage_account_name = "acmp2400storageaccount"
+  container_name = "big-tf-state-acmp2400"
+  key = "jackpederson.tfstate"
+  use_azuread_auth = true
+}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_container_registry" "jackpederson_acr" {
+  name = "acrjackpederson2400"
+  resource_group_name = "rg-jackpederson"
+  location = "Central US"
+  sku = "Basic"
+  admin_enabled = false
+}
+
+resource "azurerm_container_group" "teacher-aci" {
+  name = "aci-jackpederson-acmp"
+  location = "Central US"
+  resource_group_name = "rg-jackpederson"
+  ip_address_type = "Public"
+  dns_name_label = "acmp-jackpederson-instance"
+  os_type = "Linux"
+
+  container {
+    name   = "final"
+    image  = "jackpedersonacmp2400.azurecr.io/final:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 8000
+      protocol = "TCP"
+    }
+    secure_environment_variables = {
+      "DJANGO_SECRET_KEY" = var.DJANGO_SECRET_KEY_PROD
+    }
+  }
+
+  image_registry_credential {
+    server   = "jackpedersonacmp2400.azurecr.io"
+    username = var.ARM_CLIENT_ID
+    password = var.ARM_CLIENT_SECRET
+  }
+}
